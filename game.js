f3ef4b3e78806306a719686b51b15bb2d707c104
Graphics <Graphics@SM.local>
2018-10-12 14:27:01 -0400

    Added game logic to matching and winning

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
-const flipCount = 0;
-const moveCount = 0;
-const timer = 0;
-const stars = 5;
+let flipCount = 0;
+let moveCount = 0;
+let matched = 0;
+let timer = 0;
+let stars = 5;
 let initArray = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 const gridContainer = document.getElementById('grid-container');
 const startButton = document.getElementById('start-button');
+
+let flippedCard1;
+let flippedCard2;
 
 startButton.textContent = "START";
 
@@ -12,10 +16,10 @@ function reset(){
   //shuffles initial array to provide randomness
   shuffleArray(initArray);
   //creates card elements
-  for (let i = 1; i<16; i++){
-    createCard(i, initArray[i]);
+  for (let i = 1; i<=16; i++){
+    createCard(i, initArray[i-1]);
   }
-  //para assign card number and random icon
+  //appends a card to the grid container
   function createCard(cardNumber, iconNumber){
     const newCard = document.createElement('div');
     const newOverlay = document.createElement('div');
@@ -38,35 +42,59 @@ function reset(){
 }
 
 function flip(card){
+  //makes sure user is clicking on a card
   if (card.target.nodeName === 'DIV'){
+    //does not work on already flipped cards
     if (card.target.classList.contains('card-overlay')){
       console.log('face down card was clicked');
       //reveals the hidden side of the cards by toggling class
       card.target.classList.toggle('flipped');
-    } else{
-      console.log('face up card was clicked');
-      //turns the card back over
-      card.target.querySelector('.card-overlay').classList.toggle('flipped');
+      //store id in variable
+      if (flipCount == 0){
+        flippedCard1=card.target.parentElement;
+        flipCount++;
+        return;
+      }
+      //if a card was already flipped, check if the icons match
+      if (flipCount == 1){
+        flippedCard2=card.target.parentElement;
+        if (flippedCard1.querySelector('img').src == flippedCard2.querySelector('img').src){
+          match();
+        }else{
+          setTimeout(noMatch,500);
+        }
+      }
     }
   }
-  //does not work on already flipped cards
-  //if the flipCount equals 2 --> match() or noMatch()
-  //if flip count equals 1 --> store id in variable
 }
 
 function match(){
-  //the cards stay turned over (dont toggle class again)
-  //resets flipCount
-  //increased matched variable
-  //increses moveCount
-  //if matched equals 8 --> endGame()
+  //if matched equals 8 end the game
+  console.log('match');
+  //increase matched variable
+  matched++;
+  if (matched == 8){
+    endGame();
+    return;
+  }else{
+    //the cards stay turned over (dont toggle class again)
+    //resets flipCount
+    flipCount = 0;
+    //increse moveCount
+    moveCount++;
+  }
 }
 
 function noMatch(){
+  console.log('no match');
   //the cards flip back over (toggle class again)
+  flippedCard1.querySelector('.card-overlay').classList.toggle('flipped');
+  flippedCard2.querySelector('.card-overlay').classList.toggle('flipped');
   //increses moveCount
+  moveCount++;
   //resets flipCount
-  //decreaseStars()
+  flipCount = 0;
+  decreaseStars();
 }
 
 function decreaseStars(){
@@ -74,6 +102,7 @@ function decreaseStars(){
 }
 
 function endGame(){
+  console.log('yah fucking won!!');
   //show winning message and game stats
   //ends timer
 }
